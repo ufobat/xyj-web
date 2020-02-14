1968f3ff649bb055529f73487fda3f2da3c52b9f
Martin Barth <martin@senfdax.de>
2020-02-14 19:19:40 +0100

    added start.spring.io files

---
+++ src/main/kotlin/de/senfdax/xyjweb/XyjWebApplication.kt
@@ -0,0 +1,11 @@
+package de.senfdax.xyjweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XyjWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<XyjWebApplication>(*args)
+}
---
+++ src/test/kotlin/de/senfdax/xyjweb/XyjWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.senfdax.xyjweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XyjWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
