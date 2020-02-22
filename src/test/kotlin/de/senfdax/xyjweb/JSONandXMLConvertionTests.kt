36356a1cc3e057876229c2ad9da1c532dd700de9
Martin Barth <martin@senfdax.de>
2020-02-22 22:38:54 +0100

    even more strange tests

--- src/test/kotlin/de/senfdax/xyjweb/JSONandXMLConvertionTests.kt
+++ src/test/kotlin/de/senfdax/xyjweb/JSONandXMLConvertionTests.kt
@@ -1,7 +1,12 @@
 package de.senfdax.xyjweb
 
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.core.format.DataFormatDetector
+import com.fasterxml.jackson.core.format.MatchStrength
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.xml.XmlFactory
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -10,21 +15,35 @@ import org.springframework.boot.test.context.SpringBootTest
 class JSONandXMLConvertionTests {
 	val xmlMapper = XmlMapper()
 	val jsonMapper = ObjectMapper()
+	val xmlString = "<a omg='1'><b foo='bar' /></a>"
+	val jsonString = "{ \"foo\": 1, \"b\": { \"bar\": \"baz\"} }"
+	val detector = DataFormatDetector(JsonFactory(), XmlFactory(), YAMLFactory())
+			.withMinimalMatch(MatchStrength.WEAK_MATCH)
+			.withOptimalMatch(MatchStrength.SOLID_MATCH)
 
 	@Test
 	fun convert_xml_to_json() {
-		val string = "<a omg='1'><b foo='bar' /></a>"
-		val node = xmlMapper.readTree(string)
+		val node = xmlMapper.readTree(xmlString)
 		val jsonString = jsonMapper.writeValueAsString(node)
 		assertEquals("{\"omg\":\"1\",\"b\":{\"foo\":\"bar\"}}", jsonString);
 	}
 
 	@Test
 	fun convert_json_to_xml() {
-		val string = "{ \"foo\": 1, \"b\": { \"bar\": \"baz\"} }"
-		val node = jsonMapper.readTree(string)
+		val node = jsonMapper.readTree(jsonString)
 		val xmlString = xmlMapper.writeValueAsString(node);
 		assertEquals("<ObjectNode><foo>1</foo><b><bar>baz</bar></b></ObjectNode>", xmlString)
 	}
 
+	@Test
+	fun detect_json() {
+		val match = detector.findFormat(jsonString.toByteArray())
+		assertEquals(match.matchedFormatName, JsonFactory().formatName)
+	}
+
+	@Test
+	fun detect_yml() {
+		val match = detector.findFormat(xmlString.toByteArray())
+		assertEquals(match.matchedFormatName, XmlFactory().formatName)
+	}
 }
